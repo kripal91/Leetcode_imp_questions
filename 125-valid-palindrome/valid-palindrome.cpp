eac4e1962e3afeefa0601cee5815466247f2d9c7
Kripal Singh <136116119+kripal91@users.noreply.github.com>
2024-02-25 17:37:44 +0530

    Time: 5 ms (53.71%) | Memory: 9 MB (19.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp="";
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || 
-            (s[i]>='0' && s[i]<='9'))
-            temp.push_back(s[i]);
+        string t("");
+        for(char &c:s){
+            if((c>='a' && c<='z' ) || (c>='A' && c<='Z') || (c>='0' && c<='9'))
+            t.push_back(tolower(c));
         }
-        string t1= temp;
-        transform(t1.begin(),t1.end(),t1.begin(),::tolower);
-        transform(temp.begin(),temp.end(),temp.begin(),::tolower);
-        reverse(t1.begin(),t1.end());
-        return t1==temp;
+        int l=0,r=t.size()-1;
+        while(l<r){
+            if(t[l]!=t[r])
+            return false;
+            l++; r--;
+        }
+        return true;
     }
 };
